15e15f38ead877eb81195575afb24300fa9144b9
emivargxs <emile.bordinvargas@gmail.com>
2025-03-29 21:09:12 -0300

    Versão2-recebendoArgumentosPelaLinhaDeComando

--- src/App.java
+++ src/App.java
@@ -1,20 +1,24 @@
+import java.util.Scanner;
 public class App{
     public static void main(String args[]){
-        Placa p1 = new Placa("Brasil","ABC1R23");
+
+        Scanner entrada = new Scanner(System.in);
+
+        System.out.println("Digite a placa: ");
+        String placa = entrada.nextLine();
+
+        System.out.println("Digite o combustivel no tanque: ");
+        double combustivel = entrada.nextDouble();  
+
+        System.out.println("Distancia percorrida: ");
+        double distancia = entrada.nextDouble();
+
+        Veiculo carro = new Veiculo(new Placa(placa,""));
+        carro.abastece(combustivel); 
+        System.out.println("Placa: "+carro.getPlaca());
+        System.out.println("Distancia percorrida: "+carro.dirige(distancia));
+        System.out.println("Combustivel no tanque: "+carro.getCombustivelNoTanque());
+        entrada.close();
         
-        Veiculo v = new Veiculo(p1);
-        System.out.println("Dados do veiculo antes de abastecer:");
-        System.out.println(v);
-        System.out.println("----------------");
-
-        v.abastece(65);
-        System.out.println("Dados do veiculo depois de abastecer:");
-        System.out.println(v);
-        System.out.println("----------------");
-
-        v.dirige(250);
-        System.out.println("Dados do veiculo depois de percorrer 250Km:");
-        System.out.println(v);
-        System.out.println("----------------");
         }
 }
